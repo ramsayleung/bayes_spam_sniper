def6839d1482e7614378c77d9620ba6ea25a6b54
Ramsay Leung <ramsayleung@gmail.com>
2025-08-24 11:36:15 -0700

    Add a mechanism to auto reload active job

--- app/jobs/telegram_bot_worker_job.rb
+++ app/jobs/telegram_bot_worker_job.rb
@@ -1,11 +1,32 @@
 class TelegramBotWorkerJob < ApplicationJob
   queue_as :default
 
-  def perform(token)
+  def perform(token, restart: false)
+    if restart && @current_bot
+      puts "Stopping current bot instance..."
+      @current_bot.stop_bot
+    end
+    
     puts "Starting bot..."
     Rails.logger.info "Starting bot..."
+    
+    @current_bot = TelegramBotter.new
+    setup_code_reloader(token) if Rails.env.development?
+    @current_bot.start_bot(token)
+  end
 
-    telegram_bot = TelegramBotter.new
-    telegram_bot.start_bot(token)
+  private
+
+  def setup_code_reloader(token)
+    Thread.new do
+      loop do
+        sleep 1
+        if Rails.application.reloader.check!
+          puts "Code changed, scheduling bot restart..."
+          TelegramBotWorkerJob.perform_later(token, restart: true)
+          break # Exit this monitoring thread
+        end
+      end
+    end
   end
 end
