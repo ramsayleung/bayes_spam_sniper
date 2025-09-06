305460a1995feafba2f92d55885c44904740b63c
Ramsay Leung <ramsayleung@gmail.com>
2025-09-06 00:15:11 -0700

    fix: use group_id and sender_chat_id as unique keys before create new banned user

--- app/jobs/telegram_post_worker_job.rb
+++ app/jobs/telegram_post_worker_job.rb
@@ -1,4 +1,5 @@
 class TelegramPostWorkerJob < ApplicationJob
+  "background worker to execute job after specific event"
   queue_as :low_priority
 
   module PostAction
@@ -28,13 +29,14 @@ class TelegramPostWorkerJob < ApplicationJob
     I18n.with_locale("en") do
       bot.api.ban_chat_member(chat_id: trained_message.group_id, user_id: trained_message.sender_chat_id)
       BannedUser.find_or_create_by!(
-        group_name: trained_message.group_name,
         group_id: group_id,
-        sender_chat_id: user_id,
-        sender_user_name: user_name,
-        spam_message: trained_message.message,
-        message_id: trained_message.message_id
-      )
+        sender_chat_id: user_id
+      ) do |banned_user|
+        banned_user.group_name = trained_message.group_name
+        banned_user.sender_user_name = user_name
+        banned_user.spam_message = trained_message.message
+        banned_user.message_id = trained_message.message_id
+      end
       bot.api.send_message(
         chat_id: group_id,
         text: I18n.t("telegram_bot.handle_regular_message.ban_user_message", user_name: user_name, user_id: user_id),
