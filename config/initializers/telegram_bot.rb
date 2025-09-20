fe9bb75cd35d95f2d0eb0a6cf93107874ceff808
Ramsay Leung <ramsayleung@gmail.com>
2025-09-19 17:13:59 -0700

    fix problem that bot doesn't start at the first deployment

--- config/initializers/telegram_bot.rb
+++ config/initializers/telegram_bot.rb
@@ -1,10 +1,1 @@
-# Only run this code when the Rails server is starting.
-# This prevents it from running during `assets:precompile`, `db:migrate`, etc.
-
-if defined?(Rails::Server)
-  require "telegram/bot"
-  Rails.application.config.after_initialize do
-    token = Rails.application.credentials.dig(:telegram_bot_token)
-    TelegramBotWorkerJob.perform_later(token) if token
-  end
-end
+# Bot is now started from the job worker via bin/bot script
