48cceb05993d60aa2bcd1e945131a9ca20e5fd23
Ramsay Leung <ramsayleung@gmail.com>
2025-11-21 20:55:05 -0800

    add logging to keep track of time job takes to retrain

--- app/jobs/classifier_trainer_job.rb
+++ app/jobs/classifier_trainer_job.rb
@@ -3,7 +3,9 @@ class ClassifierTrainerJob < ApplicationJob
   queue_as :training
 
   def perform(trained_messages)
+    start_time = Time.current
     Rails.logger.info "Retraining classifiers."
+
     trained_message_ids = trained_messages.map { |data| data["id"] }
     trained_messages = TrainedMessage.where(id: trained_message_ids)
 
@@ -12,17 +14,30 @@ class ClassifierTrainerJob < ApplicationJob
     message_content_messages = trained_messages.select(&:message_content?)
 
     if user_name_messages.any?
+      username_start_time = Time.current
+
       GroupClassifierState.username.find_each do |classifier|
         spam_classifier = SpamClassifierService.new(classifier.group_id, classifier.group_name)
         spam_classifier.train_batch(user_name_messages)
       end
+
+      username_duration = Time.current - username_start_time
+      Rails.logger.info "Username classifier training completed in #{username_duration.round(2)} seconds with #{user_name_messages.count} messages."
     end
 
     if message_content_messages.any?
+      group_start_time = Time.current
+
       GroupClassifierState.for_group.find_each do |classifier|
         spam_classifier = SpamClassifierService.new(classifier.group_id, classifier.group_name)
         spam_classifier.train_batch(message_content_messages)
       end
+
+      group_duration = Time.current - group_start_time
+      Rails.logger.info "Group classifier training completed in #{group_duration.round(2)} seconds with #{message_content_messages.count} messages."
     end
+
+    total_duration = Time.current - start_time
+    Rails.logger.info "Classifier training job completed in #{total_duration.round(2)} seconds."
   end
 end
